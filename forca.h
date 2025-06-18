13819a1956885ab6402c7c164cc16264ed49727f
felipe vidal <136502385+vvvvvdal@users.noreply.github.com>
2025-06-18 18:20:45 -0300

    Add files via upload

---
+++ forca.h
@@ -0,0 +1,17 @@
+#define TAMANHO_PALAVRA 20
+
+void menu();
+void titulo();
+void chutar();
+void desenhaForca();
+void escolhePalavra();
+int acertou();
+int enforcou();
+int jaChutou(char resposta);
+void resetarVariaveis();
+void imprimirBancoPalavras();
+int verificarBancoPalavra();
+void palavraMaiuscula(char* string);
+void adicionarPalavra();
+int chutesErrados();
+void iniciarJogo();
